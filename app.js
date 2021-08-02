b3960705e08426a12e6d15727e35461f6b82eb3f
Bian Péter <bianpeti@gmail.com>
2021-08-02 15:28:09 +0200

    jwt token update

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const fetch = require('node-fetch');
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+require('dotenv').config();
 
+/*
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
   proxyReq.setHeader('X-Special-Proxy-Header', 'Super Special Proxy Header Thing');
 });
+*/
 
+app.use(cors())
 
+proxy.on('proxyReq', async function (proxyReq, req, res, options) {
+
+  jwt.verify(
+    req.headers['authorization'],
+    process.env.JWT_SECRET_KEY,
+    function (err, decoded) {
+      if (err) {
+        console.log(err)
+        proxyReq.removeHeader('x-user-id')
+      }
+      else proxyReq.setHeader('x-user-id', decoded.user.email);
+    }
+  );
+
+});
+
+app.use('/api/todo*', async (req, res, next) => {
+	proxy.web(req, res, { target: 'http://localhost:3001/api/userboard' });
+});
 
 app.get('/api/login', async (req,res) => {
   const code = req.query.code;
@@ -19,12 +42,11 @@ app.get('/api/login', async (req,res) => {
   const response = await fetch('https://oauth2.googleapis.com/token', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-    },
+      'Content-Type': 'application/json'},
     body: JSON.stringify({
       code,
-      client_id: `644408839831-00b0crm2af8o30hr55diqm36lfd0sv36.apps.googleusercontent.com`,
-      client_secret: `sW-Omr2BCSnAjfWYa9KJDnGH`,
+      client_id: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      client_secret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
       redirect_uri: 'http://localhost:3000/api/login',
       grant_type: `authorization_code`,
     }),
@@ -36,11 +58,14 @@ app.get('/api/login', async (req,res) => {
 
   console.log(userInfo);
 
+  let token = jwt.sign({user: userInfo}, process.env.JWT_SECRET_KEY,{
+    expiresIn: 60 * 60,
+  })
+
+  console.log(token)
+
   res.status(201).json({ message: 'Logged In'});
 });
 
-app.use('/api/todo*', async (req, res, next) => {
-	proxy.web(req, res, { target: 'http://localhost:3001/api/userboard/test' });
-});
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+module.exports = app;
--- start.js
+++ start.js
@@ -1,6 +1,4 @@
-/*const app = require('./app.js');
+const app = require('./app.js');
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-ű
-*/
